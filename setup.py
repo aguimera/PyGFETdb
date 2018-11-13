96b156e6ebadaa8203bd3e65fddf3d35deadadae
aguimera <anton.guimera@csic.es>
2018-11-13 15:45:25 +0100

    new ver

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ Created on Thu Dec 21 18:30:42 2017
 
 from setuptools import setup, find_packages
 
-_version = '0.1.3'
+_version = '0.1.4'
 
 long_description = """
                    Library for electrphysiological analysis based on neo                   
