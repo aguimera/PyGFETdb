583f834db7dbc7997cf111d2033ee10bc29540ab
aguimera <anton.guimera@csic.es>
2019-01-23 11:13:08 +0100

    fill properties done

--- PhyREC/GuiRecordView/GuiPltControl.py
+++ PhyREC/GuiRecordView/GuiPltControl.py
@@ -21,14 +21,16 @@ import matplotlib.pyplot as plt
 import quantities as pq
 
 
-AxProp = ('ylim',
-              'ylabel',
-              'visible',
-              'autoscaley_on')
+AxesProp = ('ylim',
+            'ylabel',
+            'xlabel',
+            'visible',
+            'autoscaley_on')
 
-AxXprop = ('visible', )
+AxisProp = ('visible',
+            'scale',
+           )
 
-AxYprop = ('visible', )
 
 class GuiPltControl(QtWidgets.QMainWindow):
     def __init__(self, PlotSlot):
@@ -38,39 +40,66 @@ class GuiPltControl(QtWidgets.QMainWindow):
         uic.loadUi(uipath, self)
 
         self.setWindowTitle('Plot Control')
-        self.TreeCtr.setColumnCount(2)
+        self.TreeCtr.setColumnCount(3)
 
 #            parent.setFlags(parent.flags() | Qt.ItemIsTristate | Qt.ItemIsUserCheckable)
         for iax, ax in enumerate(PlotSlot.Axs):
             AxParent = QTreeWidgetItem(self.TreeCtr)
-            AxParent.setText(0, "Axes {}".format(iax))
-
-            props = ax.properties()
-            for p in AxProp:
-                AxChild = QTreeWidgetItem(AxParent)
-                AxChild.setFlags(AxChild.flags() | Qt.ItemIsEditable)
-                AxChild.setText(0, p)
-                pval = props[p]
-                if type(pval) == bool:
-                    AxChild.setCheckState(1, pval)
-                else:
-                    AxChild.setData(1, Qt.EditRole, pval)
+            AxParent.setText(0, "Axes")
+            AxParent.setText(1, PlotSlot.SlotsInAxs[ax][0].name)
+            AxParent.setData(2, Qt.EditRole, iax)
             
-            for sl in PlotSlot.SlotsInAxs[ax]:
+            self.AddAxProp(ParentItem=AxParent,
+                           PropsToAdd=AxesProp,
+                           Properties=ax.properties())
+
+            AxYParent = QTreeWidgetItem(AxParent)
+            AxYParent.setText(0, "YAxis")
+            self.AddAxProp(ParentItem=AxYParent,
+                           PropsToAdd=AxisProp,
+                           Properties=ax.get_yaxis().properties())
+            
+            AxXParent = QTreeWidgetItem(AxParent)
+            AxXParent.setText(0, "XAxis")
+            self.AddAxProp(ParentItem=AxXParent,
+                           PropsToAdd=AxisProp,
+                           Properties=ax.get_yaxis().properties())
+
+
+            
+            for isl, sl in enumerate(PlotSlot.SlotsInAxs[ax]):
                 WvChild = QTreeWidgetItem(AxParent)
-                WvChild.setText(0, sl.name)
+                WvChild.setText(0, str(type(sl)).split('.')[-1])
+                WvChild.setText(1, sl.name)
+                WvChild.setData(2, Qt.EditRole, isl)
                 for sp, sv in sl.LineKwargs.items():
                     WvPChild = QTreeWidgetItem(WvChild)
                     WvPChild.setFlags(WvPChild.flags() | Qt.ItemIsEditable)
-                    
-                    WvPChild.setText(0, sp)
-#                    WvPChild.setText(1, str(sv))
-#                    if type(sv) == bool:
-#                        WvPChild.setCheckState(1, sv)
-#                    else:
-                    WvPChild.setData(1, Qt.EditRole, sv)
+                    self.AddItemData(WvPChild, sp, sv)
 
         self.TreeCtr.itemChanged.connect(self.ItemChanged)
+
+    def AddAxProp(self, ParentItem, PropsToAdd, Properties):
+        for p in PropsToAdd:
+            AxChild = QTreeWidgetItem(ParentItem)
+            AxChild.setFlags(AxChild.flags() | Qt.ItemIsEditable)
+            self.AddItemData(AxChild, p, Properties[p])
+
+    def AddItemData(self, Item, DataName, DataValue):
+        Item.setText(0, DataName)
+        if type(DataValue)==tuple:
+            for iv, val in enumerate(DataValue):
+                Child = QTreeWidgetItem(Item)
+                Child.setFlags(Child.flags() | Qt.ItemIsEditable)
+                self.AddItemData(Child, str(iv), val)
+            return
+        elif type(DataValue)==str:
+            D = DataValue
+        elif type(DataValue)==bool:
+            D = DataValue
+        else:
+            D = float(DataValue)
+        Item.setData(1, Qt.EditRole, D)
 
     def ItemChanged(self, item, column):
         print(item.text(0), item.data(1, Qt.EditRole))
@@ -160,6 +189,12 @@ if __name__ == "__main__":
                           AutoScale=False)
 
     splt.PlotChannels(Time=Twind)
+    
+    
+    a = splt.Axs[0]
+    prop = a.properties()
+    xax = a.get_xaxis()
+
 
     main(splt)
 
