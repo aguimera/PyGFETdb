2b107b166624e4c6dd4ab2c6db2ff8142c918f21
Andrés García-Camino <57015395+dragc@users.noreply.github.com>
2019-11-02 22:04:17 +0100

    Updated for proper autodoc creation

--- PyGFETdb/__init__.py
+++ PyGFETdb/__init__.py
@@ -8,8 +8,7 @@ import PyGFETdb.QuantityClass as quantities
 
 qty = quantities.QuantityClass()  # Activates Quantities support globally
 """
-    The preferred method to deactivate Quantity support is calling *locally* the following method:
-      qty.setActive(False)
+    The preferred method to deactivate Quantity support is calling *locally* the following method: qty.setActive(False)
 """
 
 # qty = quantities.QuantityClass(False)  # Deactivates Quantities support globally
