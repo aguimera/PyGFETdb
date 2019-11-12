0f6c6835679b15c18ec28eca2d8aea4195135cd5
Andrés García-Camino <57015395+dragc@users.noreply.github.com>
2019-11-12 22:51:59 +0100

    Cleaned a bit the code. Separated the functions in several files. Now works per waffer/type and type/waffer

--- PyGFETdb/GlobalFunctions.py
+++ PyGFETdb/GlobalFunctions.py
@@ -4,11 +4,19 @@
 
 Global Functions that do not fit in the previous files.
 """
-import matplotlib.pyplot as plt
-import numpy as np
 
-from PyGFETdb import qty
+import quantities as pq
 
+import PyGFETdb.DBSearch as DbSe
+from PyGFETdb import qty, multithrds, Multiprocessing as mp
+
+# MULTIPROCESSING INITIALIZATION #################################################
+if multithrds:
+    search = mp.SearchDB_MP
+    getparams = mp.GetParams_MP
+else:
+    search = mp.SearchDB
+    getparams = mp.GetParams
 
 def updateDictOfLists(dict, key, value):
     """
@@ -42,163 +50,39 @@ def updateDictOfDicts(dict, key1, key2, value):
     else:
         k[key2] = value
 
-def _BoxplotValsGroup(ax, Col, iGroup, Vals, **kwargs):
-    bplt = ax.boxplot(Vals,
-                      positions=(iGroup,),
-                      patch_artist=True,  # fill with color
-                      widths=0.75,
-                      sym='+',
-                      labels=('',),
-                      #                      notch=True,
-                      )
 
-    for element in ('boxes', 'whiskers', 'fliers', 'means', 'medians', 'caps',):
-        plt.setp(bplt[element], color=Col)
-
-    for fl in bplt['fliers']:
-        fl.set_markeredgecolor(Col)
-
-    for patch in bplt['boxes']:
-        patch.set(facecolor=Col)
-        patch.set(alpha=0.5)
-
-    return bplt
+def DBSearchPerWaferAndType(GrBase, args):
+    GrWs = DbSe.GenGroups(GrBase, 'Wafers.Name', LongName=False)
+    ResultsParams = {}
+    for iWf, (Grwn, Grwc) in enumerate(GrWs.items()):
+        GrTypes = DbSe.GenGroups(Grwc, 'TrtTypes.Name', LongName=False)
+        ResultsDB = search(GrTypes)
+        ResultsParams[Grwn] = getparams(ResultsDB, GrTypes, args)
+    return GrWs, ResultsParams
 
 
-def _closeBoxplotValsGroup(ax, xPos, xLab, xlabel=None, ylabel=None, title='', legendtitle='', handles=None, **kargs):
-    plt.xticks(xPos, xLab, rotation=45, fontsize='small')
-    ax.set_ylabel(ylabel=ylabel, fontsize='large')
-
-    ax.set_xlabel(xlabel, fontsize='large')
-    ax.set_title(label=title, fontsize='large')
-
-
-def _PlotValsGroup(Ax, xLab, xPos, iGr, Grn, vals,
-                   Col=None, Boxplot=False, ParamUnits=None, **kwargs):
-
-    if vals is not None:  # and len(vals) >0:
-        if Boxplot:
-            Ax.boxplot(vals.transpose(), positions=(iGr + 1,))
-            xPos.append(iGr + 1)
-        else:
-            Ax.plot(np.ones(len(vals)) * iGr, vals, '.', label=(Grn,), color=Col)
-            xPos.append(iGr)
-            xLab.append(Grn)
-    else:
-        print('Empty data for: ', Grn)
+def DBSearchPerType(GrBase, args):
+    GrTypes = DbSe.GenGroups(GrBase, 'TrtTypes.Name', LongName=False)
+    ResultsParams = {}
+    for iType, (nType, cType) in enumerate(GrTypes.items()):
+        GrWfs = DbSe.GenGroups(cType, 'Wafers.Name', LongName=False)
+        ResultsDB = search(GrWfs)
+        ResultsParams[nType] = getparams(ResultsDB, GrWfs, args)
+    return GrTypes, ResultsParams
 
 
-def _closePlotValsGroup(Ax, xLab, xPos, qtys=None, ParamUnits=None,
-                        title=None, legendTitle=None, handles=None, xlabel='', **kwargs):
-    units = kwargs.get('Units')
-    plt.xticks(xPos, xLab, rotation=45, fontsize='small')
-
-    Ax.set_xlabel(xlabel, fontsize='large')
-
-    param = kwargs.get('Param')
-
-    if ParamUnits is not None:
-        Ax.set_ylabel(param + '[' + ParamUnits + ']')
-    else:
-        Ax.set_ylabel(param)
-
-    if qty.isActive() and qtys is not None:
-        qtyunits = qty.getQuantityUnits(qtys)
-        if qtyunits:
-            Ax.set_ylabel(param + '[' + qtyunits + ']')
-        elif units is not None:
-            Ax.set_ylabel(param + '[' + units + ']')
-
-    Ax.grid()
-    Ax.ticklabel_format(axis='y', style='sci', scilimits=(2, 2))
-    if len(xPos) > 1:
-        Ax.set_xlim(min(xPos) - 0.5, max(xPos) + 0.5)
-    if 'Vgs' in kwargs and 'Vds' in kwargs:
-        title = 'Vgs {} Vds {}'.format(kwargs['Vgs'], kwargs['Vds'])
-        plt.title(title)
-        plt.tight_layout()
-    if 'xscale' in list(kwargs.keys()):
-        Ax.set_xscale(kwargs['xscale'])
-    if 'yscale' in list(kwargs.keys()):
-        Ax.set_yscale(kwargs['yscale'])
-
-    Ax.set_title(title, fontsize='large')
-    if legendTitle or handles:
-        if legendTitle:
-            legend = legendTitle
-        else:
-            legend = ''
-        if handles:
-            colors = handles
-        else:
-            colors = None
-        Legend(Ax, legend, colors)
-
-
-def Legend(Ax, legend, handles):
-    chartBox = Ax.get_position()
-    Ax.set_position([chartBox.x0, chartBox.y0 + chartBox.y0 * 1.3, chartBox.width * 0.8, chartBox.height * 0.8])
-    Ax.legend(title=legend, handles=handles, markerscale=10, loc='upper right', fontsize='small',
-              bbox_to_anchor=(0.89, 0.5, 0.5, 0.5), shadow=True, ncol=1)
-
-
-def PlotGroup(ResultsParams, Group, arguments):
-    """
-
-    :param ResultsParams: The results of a search in the database
-    :param Group: A group of conditions to analyse
-    :param args: Arguments for getting the parameters
-    :param kwargs:
-    :return: A dict of args of dicts of groupnames and parameter found in a previous search
-    """
+def DataClassification(GrWs, ResultsParams):
+    clssfResults = {}
     Results = {}
-    for iarg, (karg, arg) in enumerate(arguments.items()):
-        Results[karg] = {}
-        fig, Ax = plt.subplots()
-        xLab = []
-        xPos = []
-        qtys = None
-        for iGr, (Grn, Grc) in enumerate(sorted(Group.items())):
-            argRes = ResultsParams.get(karg)
-            if argRes is not None:
-                ParamData = argRes.get(Grn)
-                if ParamData is not None:
-                    Results[karg][Grn] = ParamData
-                    if qty.isActive():
-                        qtys = np.array(ParamData)
-                        ParamData = qty.flatten(ParamData)
-                    ParamData = np.array(ParamData)
-                    _PlotValsGroup(Ax, xLab, xPos, iGr, Grn, ParamData, **arg)
-        _closePlotValsGroup(Ax, xLab, xPos, qtys, **arg)
-    return Results
-
-
-def PlotResults(Results, arguments, Colors=None, handles=None, legendTitle=None, xlabel=None, **kwargs):
-    """
-
-    :param Results: The results of a search in the database
-    :param Group: A group of conditions to analyse
-    :param args: Arguments for getting the parameters
-    :param kwargs:
-    :return: A dict of args of dicts of groupnames and parameter found in a previous search
-    """
-    for iarg, (karg, arg) in enumerate(Results.items()):
-        fig, Ax = plt.subplots()
-        xLab = []
-        xPos = []
-        qtys = None
-        icolor = 0
-        pos = 0
-        for iGr, (Grn, Grc) in enumerate(sorted(arg.items())):
-            for iGr2, (Grn2, ParamData) in enumerate(sorted(Grc.items())):
-                if ParamData is not None:
-                    if qty.isActive():
-                        qtys = np.array(ParamData)
-                        ParamData = qty.flatten(ParamData)
-                        ParamData = np.array(ParamData)
-                        _PlotValsGroup(Ax, xLab, xPos, pos, Grn2, ParamData, Colors[icolor], **arguments[karg])
-                        pos += 1
-            icolor += 1
-        _closePlotValsGroup(Ax, xLab, xPos, qtys, handles=handles, legendTitle=legendTitle,
-                            xlabel=xlabel, **arguments[karg])
-    return Results
+    for iWf, (Grwn, Grwc) in enumerate(GrWs.items()):
+        for iarg, (narg, arg) in enumerate(sorted(ResultsParams.get(Grwn).items())):  # Params
+            for iType, (TGrn, TGrc) in enumerate(arg.items()):
+                quantities = TGrc  # Param 0
+                if qty.isActive():
+                    Vals = pq.Quantity(quantities)  # np.array also works fine
+                else:
+                    Vals = quantities * 1e6
+                if Vals is not None:
+                    updateDictOfDicts(Results, Grwn, TGrn, Vals)
+            clssfResults[narg] = Results
+    return clssfResults
--- examples/DifferentTypes.py
+++ examples/DifferentTypes.py
@@ -7,13 +7,12 @@
 """
 
 import matplotlib.pyplot as plt
-import numpy as np
-import quantities as pq
 from matplotlib.patches import Patch
 
 import PyGFETdb.DBSearch as DbSe
 import PyGFETdb.GlobalFunctions as g
-from PyGFETdb import qty, multithrds, Multiprocessing as mp
+from PyGFETdb import PlotFunctions as plot
+from PyGFETdb import multithrds, Multiprocessing as mp
 
 # MULTIPROCESSING INITIALIZATION #################################################
 if multithrds:
@@ -23,110 +22,6 @@ else:
     search = mp.SearchDB
     getparams = mp.GetParams
 
-
-def DBSearchPerWaferAndType(GrBase, args):
-    GrWs = DbSe.GenGroups(GrBase, 'Wafers.Name', LongName=False)
-    ResultsParams = {}
-    for iWf, (Grwn, Grwc) in enumerate(GrWs.items()):
-        GrTypes = DbSe.GenGroups(Grwc, 'TrtTypes.Name', LongName=False)
-        ResultsDB = search(GrTypes)
-        ResultsParams[Grwn] = getparams(ResultsDB, GrTypes, args)
-    return GrWs, ResultsParams
-
-
-def DBSearchPerType(GrBase, args):
-    GrTypes = DbSe.GenGroups(GrBase, 'TrtTypes.Name', LongName=False)
-    ResultsParams = {}
-    for iType, (nType, cType) in enumerate(GrTypes.items()):
-        GrWfs = DbSe.GenGroups(cType, 'Wafers.Name', LongName=False)
-        ResultsDB = search(GrWfs)
-        ResultsParams[nType] = getparams(ResultsDB, GrWfs, args)
-    return GrTypes, ResultsParams
-
-
-def DataClassification(GrWs, ResultsParams):
-    clssfResults = {}
-    Results = {}
-    for iWf, (Grwn, Grwc) in enumerate(GrWs.items()):
-        for iarg, (narg, arg) in enumerate(sorted(ResultsParams.get(Grwn).items())):  # Params
-            for iType, (TGrn, TGrc) in enumerate(arg.items()):
-                quantities = TGrc  # Param 0
-                if qty.isActive():
-                    Vals = pq.Quantity(quantities)  # np.array also works fine
-                else:
-                    Vals = quantities * 1e6
-                if Vals is not None:
-                    g.updateDictOfDicts(Results, Grwn, TGrn, Vals)
-            clssfResults[narg] = Results
-    return clssfResults
-
-
-def PlotPerTypeNoise(Results, handles=None, xlabel=None, legendTitle=None, Colors=None, perType="", **kwargs):
-    # PLOT 1%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fig, ax = plt.subplots()
-    xLab = []
-    xPos = []
-    pos = 0
-    for iWf, (Grwn, Grwc) in enumerate(Results.items()):
-        Col = Colors[iWf]
-        for nt, (typename, vals) in enumerate(Grwc.items()):
-            if vals is not None:
-                xPos.append(pos)
-                xLab.append(typename)
-                g._BoxplotValsGroup(ax, Col, pos, vals.transpose())
-                pos += 1
-    g._closeBoxplotValsGroup(ax, xPos, xLab, xlabel, "[uVrms]", "Noise (10Hz-1kHz) " + perType, **kwargs)
-    g.Legend(ax, legendTitle, handles)
-
-
-def PlotPerTypeYield(Results, title=None, handles=None, xlabel=None, Colors=None, legendTitle=None, **kwargs):
-    # PLOTS 2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    fig, ax2 = plt.subplots()
-    xLab = []
-    xPos = []
-    pos = 0
-
-    for iWf, (wn, dd) in enumerate(Results.items()):
-        Col = Colors[iWf]
-        temp = []
-        for iType, (Grn, Grc) in enumerate(sorted(dd.items())):  # Param 0
-            temp.append(Grc.shape[1])
-        totalWf = np.sum(temp)
-
-        work = []
-        for iType, (Grn, Grc) in enumerate(sorted(dd.items())):  # Param 0
-            work.append((Grc.shape[1] / totalWf)*100)
-            xLab.append(Grn)
-            xPos.append(pos)
-            g._BoxplotValsGroup(ax2, Col, pos, work)
-            pos += 1
-    g._closeBoxplotValsGroup(ax2, xPos, xLab, xlabel, "Yield [% x Wafer]", title, **kwargs)
-    g.Legend(ax2, legendTitle, handles)
-
-
-def PlotPerTypeYieldTotal(Results, title=None, Colors=None, xlabel=None,
-                          **kwargs):
-    # PLOT 3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fig, ax2 = plt.subplots()
-    xLab = []
-    xPos = []
-
-    temp = []
-    for nt, (typename, vtype) in enumerate(Results.items()):
-        for iWf, (nWf, cWf) in enumerate(vtype.items()):
-            temp.append(cWf.shape[1])
-    total = np.sum(temp)
-
-    work = []
-    for nt, (typename, vtype) in enumerate(Results.items()):
-        Col = Colors[nt]
-        for iWf, (nWf, cWf) in enumerate(vtype.items()):
-            xLab.append(typename)
-            xPos.append(nt)
-            work.append((cWf.shape[1] / total) * 100)
-        g._BoxplotValsGroup(ax2, Col, nt, work, **kwargs)
-    g._closeBoxplotValsGroup(ax2, xPos, xLab, xlabel, "Yield [% x Type]", title, **kwargs)
 
 
 #############################
@@ -142,55 +37,57 @@ def PlotsParams(GrBase, **arguments):
     ResultsDB = search(GrTypes)
     argParams = {'ResultsDB': dict(ResultsDB), 'GrWfs': GrTypes, 'arguments': arguments, 'args': arguments}
     ResultsParams = getparams(**argParams)
-    Vals = g.PlotGroup(ResultsParams, GrTypes, arguments)
+    Vals = plot.PlotGroup(ResultsParams, GrTypes, arguments)
     return Vals
 
 
 # END PLOTS PER WAFER ################################################################
 # """
 
-# """ # Add '#' at the beginning of this line to active
 ############################
-# PLOTS PER 1 WAFER AND TYPES
+# PLOTS PER WAFER AND TYPES
 ###########################
 def PlotsPerWaferAndTypes(GrBase, arguments, Colors=None, legendTitle=None, xlabel=None, **kwargs):
     # DATABASE SEARCH ####################################################################################
-    GrWs, ResultsParams = DBSearchPerWaferAndType(GrBase, arguments)
+    GrWs, ResultsParams = g.DBSearchPerWaferAndType(GrBase, arguments)
     # DATA CLASSIFICATION ################################################################################
-    Results = DataClassification(GrWs, ResultsParams)
+    Results = g.DataClassification(GrWs, ResultsParams)
     handles = list((Patch(color=Colors[i], label=sorted(list(GrWs.keys()))[i])
                     for i in range(0, len(list(GrWs.keys())))))
     # Plot Params
-    g.PlotResults(Results, arguments, Colors, handles=handles,
-                  legendTitle=legendTitle, xlabel=xlabel, **kwargs)
+    plot.PlotResults(Results, arguments, Colors, handles=handles,
+                     legendTitle=legendTitle, xlabel=xlabel, **kwargs)
     data = Results['arg5']
     # PLOT 1%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeNoise(data, handles=handles, Colors=Colors, perType="x Wafer", **kwargs)
+    plot.PlotPerTypeNoise(data, handles=handles, Colors=Colors, perType="x Wafer", **kwargs)
     # PLOT 2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeYield(data, Colors=Colors, title="Working SGFETs x Wafer", handles=handles, **kwargs)
+    plot.PlotPerTypeYield(data, Colors=Colors, title="Working SGFETs x Wafer", handles=handles, **kwargs)
     # PLOT 3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeYieldTotal(data, Colors=Colors, title="Working SGFETs x Wafer", handles=handles, **kwargs)
+    plot.PlotPerTypeYieldTotal(data, Colors=Colors, title="Working SGFETs x Wafer", handles=handles, **kwargs)
 
 
+############################
+# PLOTS PER TYPES
+###########################
 def PlotsPerTypes(GrBase, arguments, Colors=None, **kwargs):
     # DATABASE SEARCH ####################################################################################
-    GrTypes, ResultsParams = DBSearchPerType(GrBase, arguments)
+    GrTypes, ResultsParams = g.DBSearchPerType(GrBase, arguments)
     # DATA CLASSIFICATION ################################################################################
-    Results = DataClassification(GrTypes, ResultsParams)
+    Results = g.DataClassification(GrTypes, ResultsParams)
     # PLOTTING ######
     handles = list((Patch(color=Colors[i], label=sorted(list(GrTypes.keys()))[i])
                     for i in range(0, len(list(GrTypes.keys())))))
 
     # Plot Params
-    g.PlotResults(Results, arguments, Colors, handles=handles, **kwargs)
+    plot.PlotResults(Results, arguments, Colors, handles=handles, **kwargs)
 
     data = Results['arg5']
     # PLOT 1%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeNoise(data, Colors=Colors, handles=handles, perType="x Type", **kwargs)
+    plot.PlotPerTypeNoise(data, Colors=Colors, handles=handles, perType="x Type", **kwargs)
     # PLOT 2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeYield(data, Colors=Colors, title="Working SGFETs x Type", handles=handles, **kwargs)
+    plot.PlotPerTypeYield(data, Colors=Colors, title="Working SGFETs x Type", handles=handles, **kwargs)
     # PLOT 3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    PlotPerTypeYieldTotal(data, Colors=Colors, title="Working SGFETs x Type", handles=handles, **kwargs)
+    plot.PlotPerTypeYieldTotal(data, Colors=Colors, title="Working SGFETs x Type", handles=handles, **kwargs)
 
 
 def main():
