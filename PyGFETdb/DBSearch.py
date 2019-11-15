46a58fa0c3edc44060bb414b1302b5c33ce814c1
Andrés García-Camino <57015395+dragc@users.noreply.github.com>
2019-11-15 12:57:06 +0100

    Documentation.

--- PyGFETdb/DBSearch.py
+++ PyGFETdb/DBSearch.py
@@ -3,12 +3,13 @@
 Created on Fri Jan 12 13:12:37 2018
 
 @author: aguimera
+
 """
 import logging
-import sys
 
 import numpy as np
 import quantities as pq
+import sys
 
 import PyGFETdb.DBCore as PyFETdb
 from PyGFETdb import qty
@@ -112,46 +113,65 @@ def FindCommonValues(Parameter, Conditions, Table='ACcharacts', **kwargs):
 def GetFromDB(Conditions, Table='ACcharacts', Last=True, GetGate=True,
               OutilerFilter=None, DataSelectionConfig=None):
     """
-    Get data from data base
 
-    This function returns data which meets with "Conditions" dictionary for sql
-    select query constructor.
+        **Get data from data base**
 
-    :param: Conditions : dictionary, conditions to construct the sql select query.
-    The dictionary should follow this structure:\n
-    {'Table.Field <sql operator>' : iterable type of values}
-    Example:
+        This function returns data which meets with "Conditions" dictionary for sql
+        select query constructor.
 
-    {'Wafers.Name = ':(B10803W17, B10803W11),
-    'CharTable.IsOK > ':(0,)}
+    :param Conditions: dictionary, conditions to construct the sql select query.
 
-    :param: Table : string, optional. Posible values 'ACcharacts' or 'DCcharacts'.
-    The default value is 'ACcharacts'. Characterization table to get data
+        The dictionary should follow this structure:
 
-    The characterization table of Conditions dictionary can be indicated
-    as 'CharTable'. In that case 'CharTable' will be replaced by Table value.
+            {'Table.Field <sql operator>' : iterable type of values}
 
-    :param: Last : bolean, optional. If True (default value) just the last measured
-    data for each transistor is returned. If False, all measured data is
-    returned
+        - Example:
 
-    :param: Last : bolean, optional. If True (default value) the gate measured data
-    is also obtained
+                {'Wafers.Name = ':(B10803W17, B10803W11),'CharTable.IsOK > ':(0,)}
 
-    :param: OutilerFilter : dictionary, optional. (default 'None'),
-    If defined, dictionary to perform a statistical pre-evaluation of the
-    data. The data that are not between the p25 and p75 percentile are
-    not returned. The dictionary should follow this structure:
-    {'Param':Value, --> Characterization parameter, ie. 'Ids', 'Vrms'...
-    'Vgs':Value,   --> Vgs evaluation point
-    'Vds':Value,   --> Vds evaluationd point
-    'Ud0Norm':Boolean} --> Indicates if Vgs is normalized to CNP
+    :param Table: string, optional.
 
+        Posible values 'ACcharacts' or 'DCcharacts'.
 
-    :return: Data: Dictionary with the data arranged as follows:
-    {'Transistor Name':list of PyGFET.DataClass.DataCharAC classes}
+        The default value is 'ACcharacts'. Characterization table to get data
 
-    :return: Trts: List of transistors
+        The characterization table of Conditions dictionary can be indicated
+        as 'CharTable'. In that case 'CharTable' will be replaced by Table value.
+
+    :param Last: boolean, optional.
+
+        If True (default value) just the last measured data for each transistor is returned.
+
+        If False, all measured data is returned
+
+    :param Last: boolean, optional.
+
+        If True (default value) the gate measured data is also obtained
+
+    :param OutilerFilter: dictionary, optional.  (default 'None'),
+
+        If defined, dictionary to perform a statistical pre-evaluation of the
+        data.
+
+        The data that are not between the p25 and p75 percentile are
+        not returned.
+
+            The dictionary should follow this structure:
+
+                {'Param':Value, --> Characterization parameter, ie. 'Ids', 'Vrms'...
+
+                'Vgs':Value,   --> Vgs evaluation point
+
+                'Vds':Value,   --> Vds evaluationd point
+
+                'Ud0Norm':Boolean} --> Indicates if Vgs is normalized to CNP
+
+    :return: A Dictionary with the data arranged as follows:
+
+            {'Transistor Name':list of PyGFET.DataClass.DataCharAC classes}
+
+    :return: A List of transistors
+
 
     """
 
